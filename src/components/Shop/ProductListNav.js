51a7e57254f226bcb26ac576cc432ae642a9a327
Simon Phan <hieuphan07@gmail.com>
2023-08-26 18:38:08 +0700

    Set active type

--- src/components/Shop/ProductListNav.js
+++ src/components/Shop/ProductListNav.js
@@ -1,22 +1,33 @@
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react';
 
 import DUMMY_CATEGORIES from '../../dummyData/dummyCategories.json'
 
 import classes from './ProductListNav.module.css'
 
 const ProductListNav = () => {
+  const [activeType, setActiveType] = useState();
+  const setActiveTypeHandler = (string) => {
+    setActiveType(string)
+  }
   return (
     <nav className={classes.navbar}>
       <h4 className={classes.title}>CATEGORIES</h4>
       <ul>
+        {/* Main type */}
         {DUMMY_CATEGORIES.map(curr => <li key={curr.category}>
           <span className={classes.category}>{curr.category}</span>
           <ul>
-            {curr.type.map(type => <li key={type}>
-              <NavLink className={({ isActive }) => isActive ? classes.active : undefined}>{type}</NavLink>
+
+            {/* Sub type */}
+            {curr.type.map((type) => <li key={type}>
+              <p
+                className={activeType === type ? classes.active : undefined}
+                onClick={() => setActiveTypeHandler(type)}>{type}</p>
             </li>)}
+
           </ul>
         </li>)}
+
       </ul>
     </nav>
   )
