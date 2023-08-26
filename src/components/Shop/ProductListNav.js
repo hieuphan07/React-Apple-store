b12a86bbd40da1c70a8a77ab28b12359c4c5b7fa
Simon Phan <hieuphan07@gmail.com>
2023-08-26 17:29:53 +0700

    Adjust CSS for Shop page

--- src/components/Shop/ProductListNav.js
+++ src/components/Shop/ProductListNav.js
@@ -5,19 +5,21 @@ import DUMMY_CATEGORIES from '../../dummyData/dummyCategories.json'
 import classes from './ProductListNav.module.css'
 
 const ProductListNav = () => {
-  return <nav className={classes.navbar}>
-    <h4 className={classes.title}>CATEGORIES</h4>
-    <ul>
-      {DUMMY_CATEGORIES.map(curr => <li key={curr.category}>
-        <span className={classes.category}>{curr.category}</span>
-        <ul>
-          {curr.type.map(type => <li key={type}>
-            <NavLink className={({ isActive }) => isActive ? classes.active : undefined}>{type}</NavLink>
-          </li>)}
-        </ul>
-      </li>)}
-    </ul>
-  </nav>
+  return (
+    <nav className={classes.navbar}>
+      <h4 className={classes.title}>CATEGORIES</h4>
+      <ul>
+        {DUMMY_CATEGORIES.map(curr => <li key={curr.category}>
+          <span className={classes.category}>{curr.category}</span>
+          <ul>
+            {curr.type.map(type => <li key={type}>
+              <NavLink className={({ isActive }) => isActive ? classes.active : undefined}>{type}</NavLink>
+            </li>)}
+          </ul>
+        </li>)}
+      </ul>
+    </nav>
+  )
 }
 
 export default ProductListNav;
