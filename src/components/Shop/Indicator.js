e37d744b61e8b9088ad556da343b79a33cc0045b
Simon Phan <hieuphan07@gmail.com>
2023-08-28 23:00:31 +0700

    Update Indicator component

--- src/components/Shop/Indicator.js
+++ src/components/Shop/Indicator.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import classes from './Indicator.module.css'
+
 const Indicator = () => {
   return (
-    <div>Indicator</div>
+    <>
+      <div className={classes['indicator-container']}>
+        <div className={classes.indicator}>
+          <button>{`<<`}</button>
+          <span>1</span>
+          <button>{`>>`}</button>
+        </div>
+      </div>
+      <div className={classes['indicator-info']}>
+        <span>Showing 1-9 of 9 results</span>
+      </div>
+    </>
   )
 }
 
