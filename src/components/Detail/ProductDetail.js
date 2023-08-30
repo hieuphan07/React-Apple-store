b0477c871edadfe2154019c0e766b64fb941fb99
Simon Phan <hieuphan07@gmail.com>
2023-08-30 16:52:07 +0700

    Update UI Detail Page

--- src/components/Detail/ProductDetail.js
+++ src/components/Detail/ProductDetail.js
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import classes from './ProductDetail.module.css'
 
-const localItem = localStorage.getItem('SELECTED_PRODUCTS');
-const SELECTED_PRODUCTS = JSON.parse(localItem);
+const formatter = new Intl.NumberFormat("de-DE", {
+  style: "currency",
+  currency: "VND",
+});
 
 const ProductDetail = () => {
+  const localItem = localStorage.getItem('SELECTED_PRODUCTS');
+  const SELECTED_PRODUCTS = JSON.parse(localItem);
+
   const params = useParams();
   const id = params.productId;
 
-  const products = useSelector(state => state.products);
   const relatedProducts = SELECTED_PRODUCTS.filter(curr => curr._id.$oid !== id);
   const [productById] = SELECTED_PRODUCTS.filter(curr => curr._id.$oid === id);
-  const longDesc = productById['long_desc'].split('•');
 
+  let longDesc;
+  if (productById['long_desc'].includes('•')) {
+    longDesc = productById['long_desc'].split('•');
+  } else {
+    longDesc = productById['long_desc'].split('-');
+  }
 
   const imageList = [];
   for (let i = 1; i <= 4; i++) {
@@ -45,11 +53,11 @@ const ProductDetail = () => {
 
         <div className={classes.text}>
           <span className={classes.name}>{productById.name}</span>
-          <span className={classes.price}>{productById.price}</span>
+          <span className={classes.price}>{formatter.format(productById.price)}</span>
           <span className={classes.shortDesc}>{productById['short_desc']}</span>
-          <span className={classes.category}>{productById.category}</span>
+          <span className={classes.category}><strong>CATEGORY: </strong>{productById.category}</span>
           <div className={classes.quantity}>
-            <input type='number'></input>
+            <input type='number' min='1' placeholder='QUANTITY'></input>
             <button>Add to cart</button>
           </div>
         </div>
@@ -58,7 +66,7 @@ const ProductDetail = () => {
       {/* Long Description */}
 
       <div className={classes.longDesc}>
-        <button>DESCRIPTION</button>
+        <span>DESCRIPTION</span>
         <h3>PRODUCT DESCRIPTION</h3>
         <ul>
           {longDesc.map(spec => <li key={spec}>
@@ -74,6 +82,8 @@ const ProductDetail = () => {
         <ul>
           {relatedProducts.map((prods, ind) => <li key={prods._id.$oid}>
             <img src={prods.img1} alt={`prod-${ind + 1}`} />
+            <span>{prods.name}</span>
+            <span>{formatter.format(prods.price)}</span>
           </li>)}
         </ul>
       </div>
