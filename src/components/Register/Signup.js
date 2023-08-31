6324fa8e1d3343c96a23af2ba5fb9820a6caf659
Simon Phan <hieuphan07@gmail.com>
2023-08-31 21:16:09 +0700

    Add validation

--- src/components/Register/Signup.js
+++ src/components/Register/Signup.js
@@ -1,18 +1,45 @@
+import { useState } from 'react';
 import React from 'react'
 import { Link } from 'react-router-dom';
 
 import classes from './Signup.module.css';
 
 const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const submitHanlder = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className={classes.signup}>
       <div className={classes.wrapper}>
-        <form>
+        <form onSubmit={submitHanlder}>
           <h3>Sign Up</h3>
-          <input type='text' placeholder='Full Name' />
-          <input type='email' placeholder='Email' />
-          <input type='password' placeholder='Password' />
-          <input type='tel' pattern="[0-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{3}" placeholder='Phone' />
+          <input
+            type='text'
+            placeholder='Full Name'
+            value={name}
+            onChange={(e) => setName(e.target.value)} />
+          <input
+            type='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type='tel'
+            placeholder='Phone'
+            pattern='[0-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{3}'
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)} />
           <button>SIGN UP</button>
         </form>
         <p>Login? <Link to='/login'>Click</Link></p>
