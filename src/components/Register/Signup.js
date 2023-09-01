7497f1651fe69fd356f0ee139e5019c8e7871833
Simon Phan <hieuphan07@gmail.com>
2023-09-01 16:51:42 +0700

    Update validation for Sign up component

--- src/components/Register/Signup.js
+++ src/components/Register/Signup.js
@@ -1,17 +1,72 @@
-import { useState } from 'react';
 import React from 'react'
+import useInput from '../../hooks/useInput';
 import { Link } from 'react-router-dom';
 
 import classes from './Signup.module.css';
 
+const phonePattern = /^\d{2}[-]\d{3}[-]\d{3}[-]\d{3}$/;
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+const isPassword = (value) => value.length >= 8 && /\d/.test(value) && /[a-zA-Z]/.test(value);
+const isPhone = (value) => phonePattern.test(value);
+
 const Signup = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phone, setPhone] = useState('');
+  const {
+    value: fullNameValue,
+    isValid: fullNameIsValid,
+    hasError: fullNameHasError,
+    valueChangeHandler: fullNameChangeHandler,
+    inputBlurHandler: fullNameBlurHandler,
+    reset: resetFullName
+  } = useInput(isNotEmpty);
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail
+  } = useInput(isEmail);
+  const {
+    value: passwordValue,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPassword
+  } = useInput(isPassword);
+  const {
+    value: phoneValue,
+    isValid: phoneIsValid,
+    hasError: phoneHasError,
+    valueChangeHandler: phoneChangeHandler,
+    inputBlurHandler: phoneBlurHandler,
+    reset: resetPhone
+  } = useInput(isPhone);
+
+  let formIsValid = false;
+
+  if (fullNameIsValid && emailIsValid && passwordIsValid && phoneIsValid) {
+    formIsValid = true;
+  }
 
   const submitHanlder = (e) => {
     e.preventDefault();
+
+    if (!formIsValid) {
+      console.log('Something went wrong!')
+      console.log(fullNameIsValid, emailIsValid, passwordIsValid, phoneIsValid);
+      return;
+    };
+
+    console.log('Submitted!');
+    console.log(fullNameValue, emailValue, passwordValue, phoneValue);
+
+    resetFullName();
+    resetEmail();
+    resetPassword();
+    resetPhone();
   }
 
   return (
@@ -22,24 +77,32 @@ const Signup = () => {
           <input
             type='text'
             placeholder='Full Name'
-            value={name}
-            onChange={(e) => setName(e.target.value)} />
+            id='full-name'
+            value={fullNameValue}
+            onChange={fullNameChangeHandler}
+            onBlur={fullNameBlurHandler} />
           <input
             type='email'
             placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} />
+            id='email'
+            value={emailValue}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler} />
           <input
             type='password'
             placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)} />
+            id='password'
+            value={passwordValue}
+            onChange={passwordChangeHandler}
+            onBlur={passwordBlurHandler} />
           <input
             type='tel'
             placeholder='Phone'
-            pattern='[0-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{3}'
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)} />
+            pattern='[0-9]{2}[-][0-9]{3}[-][0-9]{3}[-][0-9]{3}'
+            id='phone'
+            value={phoneValue}
+            onChange={phoneChangeHandler}
+            onBlur={phoneBlurHandler} />
           <button>SIGN UP</button>
         </form>
         <p>Login? <Link to='/login'>Click</Link></p>
