b99a8d0f40522c969cafb3b9bdcc1da3301a81cd
Simon Phan <hieuphan07@gmail.com>
2023-09-08 09:06:19 +0700

    Add lazy function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import RootLayout from './pages/Root';
-import ErrorPage from "./pages/Error";
+// import ErrorPage from "./pages/Error";
 import HomePage, { loader as homeProductsLoader } from "./pages/Home";
-import ShopPage, { loader as shopProductsLoader } from "./pages/Shop";
-import DetailPage from "./pages/Detail";
-import CartPage from "./pages/Cart";
-import CheckoutPage from "./pages/Checkout";
-import LoginPage from "./pages/Login";
-import RegisterPage from "./pages/Register";
+// import ShopPage, { loader as shopProductsLoader } from "./pages/Shop";
+// import DetailPage from "./pages/Detail";
+// import CartPage from "./pages/Cart";
+// import CheckoutPage from "./pages/Checkout";
+// import LoginPage from "./pages/Login";
+// import RegisterPage from "./pages/Register";
+
+const Shop = lazy(() => import('./pages/Shop'));
+const Detail = lazy(() => import('./pages/Detail'));
+const Cart = lazy(() => import('./pages/Cart'));
+const Checkout = lazy(() => import('./pages/Checkout'));
+const Login = lazy(() => import('./pages/Login'));
+const Register = lazy(() => import('./pages/Register'));
+const Error = lazy(() => import('./pages/Error'));
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    errorElement: <ErrorPage />,
+    errorElement: <Suspense><Error /></Suspense>,
     children: [
       {
         index: true,
@@ -26,10 +35,10 @@ const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: <ShopPage />,
-            loader: shopProductsLoader,
+            element: <Suspense><Shop /></Suspense>,
+            loader: () => import('./pages/Shop').then(module => module.loader()),
           },
-          { path: ':productId', element: <DetailPage /> },
+          { path: ':productId', element: <Suspense><Detail /></Suspense> },
         ]
       },
       {
@@ -37,13 +46,13 @@ const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: <CartPage />
+            element: <Suspense><Cart /></Suspense>
           },
-          { path: 'checkout', element: <CheckoutPage /> },
+          { path: 'checkout', element: <Suspense><Checkout /></Suspense> },
         ]
       },
-      { path: 'login', element: <LoginPage /> },
-      { path: 'register', element: <RegisterPage /> },
+      { path: 'login', element: <Suspense><Login /></Suspense> },
+      { path: 'register', element: <Suspense><Register /></Suspense> },
     ]
   }
 ])
